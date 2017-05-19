f82b8646fc9ad01fbf6ffc103dd532e768dda71e
Steven Strybos <stevengstrybos@gmail.com>
2017-05-19 17:46:51 -0500

    Initial commit

---
+++ assets/javascript/rps.js
@@ -0,0 +1,19 @@
+console.log("here");
+
+var player1 = {
+	connected: false,
+	choice: "",
+	wins: 0,
+	losses: 0
+};
+
+var player2 = {
+	connected: false,
+	choice: "",
+	wins: 0,
+	losses: 0
+};
+
+$(document).ready(function() {
+
+});
